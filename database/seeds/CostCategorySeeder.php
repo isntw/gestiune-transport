6a420c93fd5cec6d9d6a1256064f065639eed4b9
Iustinian Monea <iustinian.monea@it-systems.ro>
2017-11-08 16:30:42 +0200

    asd

--- database/seeds/CostCategorySeeder.php
+++ database/seeds/CostCategorySeeder.php
@@ -10,8 +10,8 @@ class CostCategorySeeder extends Seeder {
      * @return void
      */
     public function run() {
+        
         $categories = ['Motorina', 'Consumabile', 'Piese', 'Manopera', 'TAXE', 'Altele'];
-
         foreach ($categories as $category => $name) {
             \App\CostCategory::create(['name' => $name,]);
         }
